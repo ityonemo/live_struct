81cb34c8a5782f916675e5917a21b7118ca85054
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2021-02-05 17:56:05 -0800

    publishing odds and ends

--- mix.exs
+++ mix.exs
@@ -7,22 +7,23 @@ defmodule LiveStruct.MixProject do
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "lets you use structs as assigns",
+      licenses: ["MIT"],
+      links: [
+        github: "https://github.com/ityonemo/live_struct"
+      ],
+      source_url: "https://github.com/ityonemo/live_struct"
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
-    [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-    ]
+    []
   end
 end
